7fd5af0826ce6e5d5d3e8bfab8c6c1c0d25e8cd9
Rahul Agarwal <me.agarwal.rahul@gmail.com>
2022-10-01 10:34:54 -0600

    practice upload

--- LED/LED.ino
+++ LED/LED.ino
@@ -14,22 +14,22 @@ void loop() {
 }
 
 void flash(){
-  digitalWrite(13,HIGH);
+  digitalWrite(11,HIGH);
   digitalWrite(2,HIGH);
   delay(100);
   digitalWrite(12,HIGH);
   digitalWrite(3,HIGH);
   delay(100);
-  digitalWrite(11,HIGH);
+  digitalWrite(13,HIGH);
   digitalWrite(4,HIGH);
   delay(100);
-  digitalWrite(13,LOW);
+  digitalWrite(11,LOW);
   digitalWrite(2,LOW);
   delay(100);
   digitalWrite(12,LOW);
   digitalWrite(3,LOW);
   delay(100);
-  digitalWrite(11,LOW);
+  digitalWrite(13,LOW);
   digitalWrite(4,LOW);
   delay(100);
   
